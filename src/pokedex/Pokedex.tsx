498031703787230f6336427b6e4d1b3bc294eee9
Rafiere <rafapgbr@gmail.com>
2023-03-18 05:57:04 -0300

    feat: add conditional rendering

--- src/pokedex/Pokedex.tsx
+++ src/pokedex/Pokedex.tsx
@@ -3,19 +3,19 @@ import React, {useState} from "react";
 const Pokedex = () => {
 
     const [currentPokemonsList, setCurrentPokemonsList] = useState<string[]>(["Pikachu", "Ditto", "Metapod"])
-    const [currentSelectedPokemon, setCurrentSelectedPokemon] = useState<string>('Pikachu')
+    const [currentSelectedPokemon, setCurrentSelectedPokemon] = useState<string>('')
 
     return (
         <div>
             <h1>Pokedex</h1>
-            <h2>Selected Pokémon: {currentSelectedPokemon}</h2>
+            <h2>Selected Pokémon: {currentSelectedPokemon ? currentSelectedPokemon : "Nenhum pokémon selecionado!"}</h2>
 
             <h2>All Pokémons: </h2>
             <ul>
                 {currentPokemonsList.map((pokemon) => {
                     return (
                     <li>
-                        <p>{pokemon}</p>
+                        <button onClick={(e) => setCurrentSelectedPokemon(pokemon)}>{pokemon}</button>
                     </li>)
                 })}
             </ul>
